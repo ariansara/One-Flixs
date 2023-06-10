6cd4bf8a18bee8be028349685decc7caf9ba7760
ariansara <102766969+ariansara@users.noreply.github.com>
2023-06-10 16:15:06 -0300

    cambio de imagenes de carusel episodeso y mas
    se cambiaron las imagenes del carrusel
    también de la pagina principal
    y por ultimo las imágenes de la pestaña de episodios

--- js/main.js
+++ js/main.js
@@ -5,24 +5,23 @@ const animes = document.querySelectorAll('.animes, .lista')
 
 var i = 0;
 var img = [];
-img[0] = "img/1.png";
-img[1] = "img/2.png";
-img[2] = "img/3.png";
-img[3] = "img/4.png";
-img[4] = "img/5.png";
-img[5] = "img/6.png";
+img[0] = "img/carrusel/ANIME.png";
+img[1] = "img/carrusel/MANGA.png";
+img[2] = "img/carrusel/OVAS.png";
+img[3] = "img/carrusel/PELICULA.png";
+
 
 function flechaIzquierda(){
     i--
     if(i == 0){
-        i = 5;
+        i = 3;
     }
     document.getElementById("ani").src = img[i];
 }
 
 function flechaDerecha(){
     i++
-    if(i == 6){
+    if(i == 4){
         i = 0;
     }
     document.getElementById("ani").src = img[i];
